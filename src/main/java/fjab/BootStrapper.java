6a7e568c262eceaff0866213895d43f41e1b3445
Francisco Alvarez <fjab76@gmail.com>
2016-05-21 21:32:00 +0100

    adding bean RestTemplate to Spring context

--- src/main/java/fjab/BootStrapper.java
+++ src/main/java/fjab/BootStrapper.java
@@ -7,6 +7,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class BootStrapper {
@@ -16,6 +17,11 @@ public class BootStrapper {
   @Autowired
   CommandLineRunner app(MovieService movieService, SongService songService){
     return new App(movieService,songService);
+  }
+
+  @Bean
+  RestTemplate restTemplate(){
+    return new RestTemplate();
   }
 
   public static void main(String[] args) {
